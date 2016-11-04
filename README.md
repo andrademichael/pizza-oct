470ceb4caae52d12ffb0f60e857b831646fbebf4
Michael E. Andrade <andrade.michael.e@gmail.com>
2016-11-04 11:29:17 -0700

    add most of the HTML content, JS to create new client list items with click handlers to show the clients details

---
+++ README.md
@@ -0,0 +1,14 @@
+objectives
+
+1.Allow the user to choose toppings and size for the pizza they'd like to order.
+2.Create a pizza object constructor with properties for toppings and size.
+3.Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
+
+BDD:
+1.User can add themselves to a list of customers
+
+2.
+3.
+4.
+5.
+6.
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,37 @@
 //backend
-
+var Client = function(name, address, paymentMethod) {
+  this.clientName = name;
+  this.address = address;
+  this.paymentMethod = paymentMethod;
+}
 
 //frontend
 
 
 //event handlers
 $(document).ready(function() {
+  //user clicks #clientInfoSubmitButton
+  $("#clientInfoForm").submit(function(event) {
+    event.preventDefault();
+    var clientInputs = function() {
+      console.log(      document.getElementsByClassName("clientInputs"));
+    };
+    var newClientName = $("#clientNameInput").val();
+    var newClientAddress = $("#clientAddressInput").val();
+    var newClientPayment = $("#clientPaymentInput").val();
+    var newClient = new Client(newClientName, newClientAddress, newClientPayment);
+    $("#clientList").append("<li>" + newClient.clientName + "</li>");
+    $("#clientList").last().click(function() {
+      debugger;
+      $("#clientNameDisplay").text(newClient.clientName);
+      $("#clientPaymentDisplay").text(newClient.paymentMethod);
+      $("#clientAddressDisplay").text(newClient.address);
+    });
+  });
 
+  //user clicks #pizzaOrderSubmitButton
+  $("#clientInfoFormGroup").submit(function(event) {
+    event.preventDefault();
+
+  });
 });
